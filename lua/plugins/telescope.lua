f8dd4a445280c23de2dc59619b2249c19d05a79c
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2024-11-19 23:11:52 +0100

    second

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,12 @@
-Thanks for the quick reply!
-This is how i did it, is it correct?
-
-return 
-
-{
-
-	[0] = 'nvim-telescope/telescope.nvim',
-
+return {
+	'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim'},
-
 	config = function()
-
 		local builtin = require("telescope.builtin")
-
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
 		vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
-
 	end
 }
