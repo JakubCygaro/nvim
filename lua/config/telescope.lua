4e7e23e31e261bdeb6d0f2d9c578402585452899
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2024-11-22 19:08:32 +0100

    devicons working

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -4,4 +4,19 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+require('telescope').setup({
+    defaults = {
 
+    },
+    mappings = {
+        i = {
+            ["<C-h>"] = "which_key"
+        }
+    },
+    pickers = {
+
+    },
+    extensions = {
+
+    }
+})
