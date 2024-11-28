1d9dd5ed1b421eaedb3f8837185552efc3c12591
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2024-11-28 21:50:38 +0100

    Got ToggleTerm to work

--- lua/after/remap.lua
+++ lua/after/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
 vim.keymap.set("n", "<leader>nh", "<cmd>:nohl<CR>")
+vim.keymap.set("n", "<leader>nt", "<cmd>:NvimTreeToggle<CR>")
 vim.keymap.set("n", "<leader><Right><Right>", "<cmd>:BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<leader><Left><Left>", "<cmd>:BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<leader>bd", "<cmd>:BufferLinePickClose<CR>")
@@ -9,15 +10,11 @@ vim.keymap.set("n", "<leader>d<Right>", "<cmd>:BufferLineCloseRight<CR>")
 vim.keymap.set("n", "<leader>d<Left>", "<cmd>:BufferLineCloseLeft<CR>")
 vim.keymap.set("n", "<leader>d<Up>", "<cmd>:BufferLineCloseOthers<CR>")
 
---function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
---end
--- _G.set_terminal_keymaps()
---vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+vim.keymap.set("n", "<leader>tt", function ()
+    return vim.v.count .. ':ToggleTerm<CR>'
+end, {expr = true})
+vim.keymap.set("n", "<leader>ts", ':TermSelect<CR>')
+--vim.keymap.set("n", "<leader>tn", ':ToggleTermSetName<CR>')
+vim.keymap.set("n", "<leader>tn", function ()
+    return vim.v.count .. ':ToggleTermSetName<CR>'
+end, {expr = true})
