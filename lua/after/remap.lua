48cc9e33fddcd55e3e0efb124a6e940270037e94
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2024-11-28 22:09:49 +0100

    okokok??

--- lua/after/remap.lua
+++ lua/after/remap.lua
@@ -1,20 +1,25 @@
 vim.g.mapleader = " "
+--Oil
 vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
+-- :nohl
 vim.keymap.set("n", "<leader>nh", "<cmd>:nohl<CR>")
+-- nvim-tree
 vim.keymap.set("n", "<leader>nt", "<cmd>:NvimTreeToggle<CR>")
+-- BufferLine
 vim.keymap.set("n", "<leader><Right><Right>", "<cmd>:BufferLineCycleNext<CR>")
 vim.keymap.set("n", "<leader><Left><Left>", "<cmd>:BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<Tab>", "<cmd>:BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<S-Tab>", "<cmd>:BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<leader>bd", "<cmd>:BufferLinePickClose<CR>")
 vim.keymap.set("n", "<leader>bp", "<cmd>:BufferLinePick<CR>")
 vim.keymap.set("n", "<leader>d<Right>", "<cmd>:BufferLineCloseRight<CR>")
 vim.keymap.set("n", "<leader>d<Left>", "<cmd>:BufferLineCloseLeft<CR>")
 vim.keymap.set("n", "<leader>d<Up>", "<cmd>:BufferLineCloseOthers<CR>")
-
+-- ToggleTerm
 vim.keymap.set("n", "<leader>tt", function ()
     return vim.v.count .. ':ToggleTerm<CR>'
 end, {expr = true})
 vim.keymap.set("n", "<leader>ts", ':TermSelect<CR>')
---vim.keymap.set("n", "<leader>tn", ':ToggleTermSetName<CR>')
 vim.keymap.set("n", "<leader>tn", function ()
     return vim.v.count .. ':ToggleTermSetName<CR>'
 end, {expr = true})
