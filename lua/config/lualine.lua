be68ba2d9d868f9d61b50aab8ee9815415b1efa6
JakubCygaro <57292664+JakubCygaro@users.noreply.github.com>
2025-01-06 00:27:35 +0100

    fix

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -23,7 +23,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch', 'diff', --[['diagnostics']]},
     lualine_c = {'filename', lsp_status },
     lualine_x = {  'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
