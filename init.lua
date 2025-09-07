17779f4bb0bbb5562f45a860e21537205e11111e
papieros <robertrugala@gmail.com>
2025-09-07 17:40:51 +0200

    document mappings

--- init.lua
+++ init.lua
@@ -1,118 +1,80 @@
-local status, err = true, nil
-status, err = pcall(require, "config.lazy")
-if not status then
-    print("Error while loading configuration file: " .. "config.lazy" .. "\n" .. err)
-end
-status, err = pcall(require, "config.oil")
-if not status then
-    print("Error while loading configuration file: " .. "config.oil" .. "\n" .. err)
-end
-status, err = pcall(require, "config.nvim-tree")
-if not status then
-    print("Error while loading configuration file: " .. "config.nvim-tree" .. "\n" .. err)
-end
-status, err = pcall(require, "config.vscode")
-if not status then
-    print("Error while loading configuration file: " .. "config.vscode" .. "\n" .. err)
-end
-status, err = pcall(require, "config.bufferline")
-if not status then
-    print("Error while loading configuration file: " .. "config.bufferline" .. "\n" .. err)
-end
-status, err = pcall(require, "config.cmp")
-if not status then
-    print("Error while loading configuration file: " .. "config.cmp" .. "\n" .. err)
-end
-status, err = pcall(require, "config.telescope")
-if not status then
-    print("Error while loading configuration file: " .. "config.telescope" .. "\n" .. err)
-end
-status, err = pcall(require, "config.lspconfig")
-if not status then
-    print("Error while loading configuration file: " .. "config.lspconfig" .. "\n" .. err)
-end
-status, err = pcall(require, "config.treesitter")
-if not status then
-    print("Error while loading configuration file: " .. "config.treesitter" .. "\n" .. err)
-end
-status, err = pcall(require, "config.undotree")
-if not status then
-    print("Error while loading configuration file: " .. "config.undotree" .. "\n" .. err)
-end
-status, err = pcall(require, "config.vim-fugitive")
-if not status then
-    print("Error while loading configuration file: " .. "config.vim-fugitive" .. "\n" .. err)
-end
-status, err = pcall(require, "config.toggleterm")
-if not status then
-    print("Error while loading configuration file: " .. "config.toggleterm" .. "\n" .. err)
-end
-status, err = pcall(require, "config.lualine")
-if not status then
-    print("Error while loading configuration file: " .. "config.lualine" .. "\n" .. err)
-end
-status, err = pcall(require, "config.nvim-dap")
-if not status then
-    print("Error while loading configuration file: " .. "config.nvim-dap" .. "\n" .. err)
-end
-status, err = pcall(require, "config.dap-ui")
-if not status then
-    print("Error while loading configuration file: " .. "config.dap-ui" .. "\n" .. err)
-end
-status, err = pcall(require, "config.neocord")
-if not status then
-    print("Error while loading configuration file: " .. "config.neocord" .. "\n" .. err)
-end
-status, err = pcall(require, "config.hex-view")
-if not status then
-    print("Error while loading configuration file: " .. "config.hex-view" .. "\n" .. err)
-end
-status, err = pcall(require, "config.lsp-signature")
-if not status then
-    print("Error while loading configuration file: " .. "config.lsp-signature" .. "\n" .. err)
-end
-status, err = pcall(require, "after.remap")
-if not status then
-    print("Error while loading configuration file: " .. "after.remap" .. "\n" .. err)
+-- configuration files to load
+local configs_to_load = {
+    "config.lazy",
+    "config.oil",
+    "config.nvim-tree",
+    "config.vscode",
+    "config.bufferline",
+    "config.cmp",
+    "config.telescope",
+    "config.lspconfig",
+    "config.treesitter",
+    "config.undotree",
+    "config.vim-fugitive",
+    "config.toggleterm",
+    "config.lualine",
+    "config.nvim-dap",
+    "config.dap-ui",
+    "config.neocord",
+    "config.hex-view",
+    "config.lsp-signature",
+    "after.remap",
+}
+
+for _, v in ipairs(configs_to_load) do
+    -- load each file via pcall so that if one plugin fails to configure that won't fuck up the whole neovim configuraton process
+    local status, err = pcall(require, v)
+    if not status then
+        print("Error while loading configuration file: " .. v .. "\n" .. err)
+    end
 end
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.number = true  -- turn on line numbers
+vim.opt.relativenumber = true -- turn on relative line numbering
 vim.opt.signcolumn = "number"
 vim.o.tabstop = 4      -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4   -- Number of spaces inserted when indenting
-vim.o.showbreak = '~ '
+vim.o.showbreak = '~ ' -- character for wrapped lines
 vim.g.move_map_keys = 0;
 
-vim.cmd('nnoremap <leader>ce  :edit $MYVIMRC<cr>')
-vim.cmd('nnoremap <leader>cs  :source $MYVIMRC<cr>')
-
-vim.cmd('nnoremap <leader>"  viW<esc>`>a"<esc>`<i"<esc>`>e')
-vim.cmd("nnoremap <leader>'  viW<esc>`>a'<esc>`<i'<esc>`>e")
-
-vim.cmd('nnoremap <leader>*  viW<esc>`>a*<esc>`<i*<esc>`>e')
-
--- vim.keymap.set("n", "<expr><leader>*", function ()
---     return "viw<esc>`>" .. vim.v.count1 .. "a*<esc>`<" .. vim.v.count1 .. "i*<esc>`>e";
--- end, { expr = true , noremap = true })
+vim.keymap.set("n", "<leader>ce", ":edit $MYVIMRC<CR>", {
+    desc = "Edit the init.lua file in a new buffer"
+})
+vim.keymap.set("n", "<leader>cs", ":source $MYVIMRC<cr>", {
+    desc = "Source the init.lua file (reload configuration)"
+})
+vim.keymap.set("n", '<leader>"', 'viW<esc>`>a"<esc>`<i"<esc>`>e', {
+    desc = 'Surrond WORD with " '
+})
+vim.keymap.set("n", "<leader>'", "viW<esc>`>a'<esc>`<i'<esc>`>e", {
+    desc = "Surrond WORD with ' "
+})
+vim.keymap.set("n", "<leader>*", "viW<esc>`>a*<esc>`<i*<esc>`>e", {
+    desc = "Surrond WORD with * "
+})
 
 -- creates a visual mode mapping that surrounds visual mode selection with a character,
 -- optionally using a different closing character if such was provided
 local function gen_surround_keymap(character, closing)
+    local description = "Surround visual selection with '" .. character "'";
+    if closing ~= nil then
+        description = description .. " and closed by '" .. closing .. "'"
+    end
     if closing ~= nil then
         vim.keymap.set("v", "<leader>" .. closing, function()
             return "<esc>`>" ..
                 vim.v.count1 .. "a" .. closing .. "<esc>`<" .. vim.v.count1 .. "i" .. character .. "<esc>`>e";
-        end, { expr = true })
+        end, { expr = true, desc = description})
     end
     closing = closing or character;
     vim.keymap.set("v", "<leader>" .. character, function()
         return "<esc>`>" .. vim.v.count1 .. "a" .. closing .. "<esc>`<" .. vim.v.count1 .. "i" .. character .. "<esc>`>e";
-    end, { expr = true })
+    end, { expr = true, desc = description})
 end
 
+-- characters for visual mode surrounding
 local surround = { '*', "'", '"', '_',
     { character = '(', closing = ')' },
     { character = '{', closing = '}' },
@@ -135,19 +97,27 @@ local function gen_surrounds(chars)
 end
 gen_surrounds(surround)
 
-vim.cmd('nnoremap L  $')
-vim.cmd('nnoremap H  ^')
+vim.keymap.set("n", "L", "$", {
+    desc = "Change the default behavior of L to the behavior of $"
+})
+vim.keymap.set("n", "H", "^", {
+    desc = "Change the default behavior of H to the behavior of ^"
+})
 
+-- add lexima rule for closing <> in html files
 vim.api.nvim_call_function('lexima#add_rule', {
     { char = '<', input_after = '>', filetype = 'html' }
 })
 
+-- add glsl recognition for treesitter for files with .vert extension
 vim.treesitter.language.register("glsl", "vert")
 
+-- highlight trailing whitespace
 vim.cmd("match DiffDelete /\\v +$/")
 
--- vim.cmd('nnoremap <leader>cd  :cd %:p:h<cr>')
-vim.keymap.set("n", "<leader>cd", "<cmd>:cd %:p:h<CR>")
+vim.keymap.set("n", "<leader>cd", "<cmd>:cd %:p:h<CR>", {
+    desc = "Change the working directory to that of the file open in the current buffer"
+})
 
 --diagnostics
 vim.diagnostic.config({ virtual_text = true, virtual_lines = false })
--- lua/after/remap.lua
+++ lua/after/remap.lua
@@ -1,73 +1,125 @@
+-- map the leader character to SPACE
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>rn", function ()
+vim.keymap.set("n", "<leader>rn", function()
     vim.o.relativenumber = not vim.o.relativenumber;
-end)
+end, {
+    desc = "Toggle relative line numbering"
+})
 --Oil
-vim.keymap.set("n", "<leader>oo", vim.cmd.Oil)
+vim.keymap.set("n", "<leader>oo", vim.cmd.Oil, { desc = "Open Oil" })
 -- :nohl
-vim.keymap.set("n", "<leader>nh", "<cmd>:nohl<CR>")
+vim.keymap.set("n", "<leader>nh", "<cmd>:nohl<CR>", {
+    desc = "Turn after search highlighting off"
+})
 -- nvim-tree
-vim.keymap.set("n", "<leader>nt", "<cmd>:NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>nt", "<cmd>:NvimTreeToggle<CR>", {
+    desc = "Open Nvim Tree"
+})
 -- BufferLine
-vim.keymap.set("n", "<leader>ll", "<cmd>:BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<leader>hh", "<cmd>:BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "<Tab>", "<cmd>:BufferLineCycleNext<CR>")
-vim.keymap.set("n", "<S-Tab>", "<cmd>:BufferLineCyclePrev<CR>")
-vim.keymap.set("n", "<leader>bc", "<cmd>:BufferLinePickClose<CR>")
-vim.keymap.set("n", "<leader>bp", "<cmd>:BufferLinePick<CR>")
-vim.keymap.set("n", "<leader>bdl", "<cmd>:BufferLineCloseRight<CR>")
-vim.keymap.set("n", "<leader>bdh", "<cmd>:BufferLineCloseLeft<CR>")
-vim.keymap.set("n", "<leader>bdj", "<cmd>:BufferLineCloseOthers<CR>")
+vim.keymap.set("n", "<leader>ll", "<cmd>:BufferLineCycleNext<CR>", {
+    desc = "Switch to buffer on the right"
+})
+vim.keymap.set("n", "<leader>hh", "<cmd>:BufferLineCyclePrev<CR>", {
+    desc = "Switch to buffer on the left"
+})
+vim.keymap.set("n", "<Tab>", "<cmd>:BufferLineCycleNext<CR>", {
+    desc = "Cycle to the next buffer"
+})
+vim.keymap.set("n", "<S-Tab>", "<cmd>:BufferLineCyclePrev<CR>", {
+    desc = "Cycle to the previous buffer"
+})
+vim.keymap.set("n", "<leader>bc", "<cmd>:BufferLinePickClose<CR>", {
+    desc = "Pick a buffer to close"
+})
+vim.keymap.set("n", "<leader>bp", "<cmd>:BufferLinePick<CR>", {
+    desc = "Pick a buffer to open"
+})
+vim.keymap.set("n", "<leader>bdl", "<cmd>:BufferLineCloseRight<CR>", {
+    desc = "Close all buffer to the right of the current"
+})
+vim.keymap.set("n", "<leader>bdh", "<cmd>:BufferLineCloseLeft<CR>", {
+    desc = "Close all buffer to the left of the current"
+})
+vim.keymap.set("n", "<leader>bdj", "<cmd>:BufferLineCloseOthers<CR>", {
+    desc = "Close all buffers except the current one"
+})
 -- ToggleTerm
-vim.keymap.set("n", "<leader>tt", function ()
+vim.keymap.set("n", "<leader>tt", function()
     return vim.v.count .. ':ToggleTerm<CR>'
-end, {expr = true})
-vim.keymap.set("n", "<leader>ts", ':TermSelect<CR>')
-vim.keymap.set("n", "<leader>tn", function ()
+end, { expr = true, desc = "Open a terminal, accepts a number if you want to open a new one" })
+vim.keymap.set("n", "<leader>ts", ':TermSelect<CR>', {
+    desc = "Select a terminal to open"
+})
+vim.keymap.set("n", "<leader>tn", function()
     return vim.v.count .. ':ToggleTermSetName<CR>'
-end, {expr = true})
+end, { expr = true, desc = "Set a name for a terminal" })
 -- ToggleTerm manager
-vim.keymap.set("n", "<leader>tm", "<cmd>:Telescope toggleterm_manager<CR>")
+vim.keymap.set("n", "<leader>tm", "<cmd>:Telescope toggleterm_manager<CR>", {
+    desc = "Find a terminal with Telescope"
+})
 
 vim.keymap.set('c', '<tab>', '<C-z>', { silent = false }) -- to fix cmp
 -- DAP
-vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end)
-vim.keymap.set('n', '<Leader>dB', function() require('dap').set_breakpoint() end)
-vim.keymap.set('n', '<Leader>dL', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
-  require('dap.ui.widgets').hover()
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end, {
+    desc = "Start a debugger session"
+})
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, {
+    desc = "(Debugging) Step over"
+})
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, {
+    desc = "(Debugging) Step into"
+})
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end, {
+    desc = "(Debugging) Step out"
+})
+vim.keymap.set('n', '<Leader>db', function() require('dap').toggle_breakpoint() end, {
+    desc = "(Debugging) Toggle breakpoint on current line"
+})
+vim.keymap.set('n', '<Leader>dB', function() require('dap').set_breakpoint() end, {
+    desc = "(Debugging) Set breakpoint on current line"
+})
+vim.keymap.set('n', '<Leader>dL',
+    function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end, {
+    desc = "(Debugging) Set breakpoint with log message"
+})
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end, {
+    desc = "(Debugging) Open repl"
+})
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end, {
+    desc = "(Debugging) Run last"
+})
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
 end)
-vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
-  require('dap.ui.widgets').preview()
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+    require('dap.ui.widgets').preview()
 end)
 vim.keymap.set('n', '<Leader>df', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.frames)
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.frames)
 end)
 vim.keymap.set('n', '<Leader>ds', function()
-  local widgets = require('dap.ui.widgets')
-  widgets.centered_float(widgets.scopes)
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.scopes)
 end)
-vim.keymap.set('n', '<Leader>dt', function ()
+vim.keymap.set('n', '<Leader>dt', function()
     require('dapui').toggle()
-end)
+end, {
+    desc = "(Debugging) Toggle UI"
+})
 -- hex view
-vim.keymap.set("n", "<leader>hv", "<cmd>:HexToggle<CR>")
+vim.keymap.set("n", "<leader>hv", "<cmd>:HexToggle<CR>", {
+    desc = "Toggle Hexview"
+})
 
 
 --diagnostics filtering
 local function set_diangnostics_keymap(lhs, level)
     local dfilter = require("after.filter_diagnostics")
     vim.keymap.set('n', lhs, function()
-        dfilter.set_level(level)
-    end,
-    { desc = "set diagnostic filter to " .. level }
+            dfilter.set_level(level)
+        end,
+        { desc = "Set diagnostic filter to " .. level }
     )
 end
 -- diagnostics filtering keybinds
@@ -75,23 +127,33 @@ set_diangnostics_keymap("<leader>E", vim.diagnostic.severity.ERROR)
 set_diangnostics_keymap("<leader>W", vim.diagnostic.severity.WARN)
 set_diangnostics_keymap("<leader>I", vim.diagnostic.severity.INFO)
 set_diangnostics_keymap("<leader>H", vim.diagnostic.severity.HINT)
-vim.keymap.set("n", "<leader>D", function ()
+vim.keymap.set("n", "<leader>D", function()
     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
 end)
 
 -- compile mode
-local compile_mode = require'compile-mode';
+local compile_mode = require 'compile-mode';
 
-vim.keymap.set('n', '<Leader>cc', ':belowright :silent :Compile<CR>')
+vim.keymap.set('n', '<Leader>cc', ':belowright :silent :Compile<CR>', {
+    desc = "Open compile mode with provided command"
+})
 
-vim.keymap.set('n', '<Leader>cr', ':belowright :silent :Recompile<CR>')
+vim.keymap.set('n', '<Leader>cr', ':belowright :silent :Recompile<CR>', {
+    desc = "Rerun compile mode with previous command"
+})
 
 vim.keymap.set('n', '<Leader>cq', function()
     compile_mode.close_buffer();
-end)
+end, {
+    desc = "Quit compile mode"
+})
 vim.keymap.set('n', '<Leader>cj', function()
     compile_mode.next_error();
-end)
+end, {
+    desc = "Jump to next error in compile mode"
+})
 vim.keymap.set('n', '<Leader>ck', function()
     compile_mode.prev_error();
-end)
+end, {
+    desc = "Jump to previous erron in compile mode"
+})
